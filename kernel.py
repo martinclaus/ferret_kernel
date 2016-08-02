da0642cca36cb04dd8c044e6d25bf30b6d8c2ef1
Martin Claus <mclaus@geomar.de>
2016-08-02 10:23:14 +0200

    Use [simple ipython wrapper kernel](http://jupyter-client.readthedocs.io/en/latest/wrapperkernels.html) as a starting point

---
+++ kernel.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 10:04:02 2016
+
+@author: mclaus
+"""
+
+from ipykernel.kernelbase import Kernel
+
+
+class FerretKernel(Kernel):
+    
+    implementation = 'Ferret'
+    implementation_version = '0.1'
+    language = 'ferret'
+    language_version = '6.8'
+    language_info = {'mimetype': 'text/plain'}
+    banner = "Ferret Kernel"
+    
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+            stream_content = {'name': 'stdout',
+                              'text': code}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+        
+        return {'status': 'ok',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+if __name__ == '__main__':
+    from ipykernel.kenrelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=FerretKernel)
