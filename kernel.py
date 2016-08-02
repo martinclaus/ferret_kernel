7f88e2e1481d4ee3e6a6caddc28d30bfe031793e
Martin Claus <mclaus@geomar.de>
2016-08-02 12:07:08 +0200

    Added spawning of subprocess and text io handling

--- kernel.py
+++ kernel.py
@@ -6,29 +6,82 @@ Created on Tue Aug  2 10:04:02 2016
 """
 
 from ipykernel.kernelbase import Kernel
+from pexpect import replwrap, EOF
+
+__version__ = '0.1'
+
+def ferret_wrapper(command="pyferret -nojnl", orig_prompt=u'yes?'):
+    ''' Start a ferret shell and retrun a :class:`REPLWrapper` object. '''
+    return replwrap.REPLWrapper(command, orig_prompt=orig_prompt,
+                                prompt_change=None)
 
 
 class FerretKernel(Kernel):
     
     implementation = 'Ferret'
-    implementation_version = '0.1'
+    implementation_version = __version__
     language = 'ferret'
     language_version = '6.8'
-    language_info = {'mimetype': 'text/plain'}
+    language_info = {'name': language,
+                     'mimetype': 'text/plain'}
     banner = "Ferret Kernel"
+
+    
+    def __init__(self, **kwargs):
+        super(FerretKernel, self).__init__(**kwargs)
+        self._start_ferret()
+
+    
+    def _start_ferret(self):
+        self.ferretwrapper = ferret_wrapper()
+
     
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
+        '''Execute the code send to the kernel '''
+
+        if not code.strip():
+            return {'status': 'ok', 'execution_count': self.execution_count,
+                    'payload': [], 'user_expressions': {}}
+        
+        interrupted = False
+        child_died = False        
+        
+        try:
+            output = self.ferretwrapper.run_command(code.strip(), timeout=None)
+        except KeyboardInterrupt:
+            self.ferretwrapper.child.sendintr()
+            interrupted = True
+            self.ferretwrapper._expect_prompt()
+            output = self.ferretwrapper.child.before
+        except EOF:
+            output = self.ferretwrapper.child.before + 'Restarting Ferret'
+            child_died = True
+            self._start_ferret()
+        
         if not silent:
             stream_content = {'name': 'stdout',
-                              'text': code}
+                              'text': output}
             self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        if interrupted:
+            return {'status': 'abort', 'execution_count': self.execution_count}
+        
+        if child_died:
+            return {'status': 'abort', 'execution_count': self.execution_count}
         
         return {'status': 'ok',
                 'execution_count': self.execution_count,
                 'payload': [],
                 'user_expressions': {},
                }
+
+#    def do_complete(self, code, cursor_pos):
+#        ''' Code completion '''
+#        # Not implemented yet
+#        super(Kernel, self).do_complete(code, cursor_pos)
+
+    
 
 if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp
